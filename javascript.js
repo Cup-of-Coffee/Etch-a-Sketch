694f3e89526d9892726517168ab732eacddb9654
Cup-of-Coffee <larry.le124@gmail.com>
2023-02-01 17:35:56 -0500

    fix css name

--- javascript.js
+++ javascript.js
@@ -7,13 +7,13 @@ Use DOM to create divs to construct the 16x16 grid with a nestled for loop.
 function createGrid(){
     const container = document.querySelector('#container');
 
-    for(x = 0; x < 16; x++){
+    for(i = 0; i < 16; i++){
         const row = document.createElement('div');
         row.classList.add('row');
 
         container.appendChild(row);
 
-        for(i = 0; i < 16; i++){
+        for(n = 0; n < 16; n++){
             const column = document.createElement('div');
             column.classList.add('column');
 
