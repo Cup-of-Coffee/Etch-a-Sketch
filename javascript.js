09307ccd0d62b1de54d92b43e38ab9025ebba41c
Cup-of-Coffee <larry.le124@gmail.com>
2023-02-01 15:46:54 -0500

    setup DOM interaction

--- javascript.js
+++ javascript.js
@@ -1,4 +1,12 @@
 
+createGrid();
+
+function createGrid(){
+    const constructContainer = document.getElementById('container');
+    constructContainer.classList.add('constructContainer');
+    constructContainer.textContent = "test.";
+}
+
 
 
 
