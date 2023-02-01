ceef4bf4accdd65865e9cddd5f55c1413631a100
Cup-of-Coffee <larry.le124@gmail.com>
2023-02-01 17:18:33 -0500

    Make a nestled loop to populate the 16x16 grid

--- javascript.js
+++ javascript.js
@@ -1,10 +1,26 @@
 
 createGrid();
 
+/*
+Use DOM to create divs to construct the 16x16 grid with a nestled for loop.
+*/
 function createGrid(){
-    const constructContainer = document.getElementById('container');
-    constructContainer.classList.add('constructContainer');
-    constructContainer.textContent = "test.";
+    const container = document.querySelector('#container');
+
+    for(x = 0; x < 16; x++){
+        const row = document.createElement('div');
+        row.classList.add('row');
+
+        container.appendChild(row);
+
+        for(i = 0; i < 16; i++){
+            const column = document.createElement('div');
+            column.classList.add('column');
+
+            row.appendChild(column);
+        }
+    }
+
 }
 
 
