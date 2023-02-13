7c65b625567fe9134e46584c253287f5a36525fa
Cup-of-Coffee <larry.le124@gmail.com>
2023-02-13 12:55:23 -0500

    Resize function

--- javascript.js
+++ javascript.js
@@ -2,7 +2,6 @@
 TODO:
 @ button to change number of squares, w/ a limit of 100 and 2.
 @ button to change color of paint.
-@ button to reset the canves.
 
 */
 
@@ -22,6 +21,12 @@ Use DOM to create divs to construct the 16x16 grid with a nestled for loop.
 function createGrid(){
     const grid = document.querySelector('#grid');
 
+    // Remove any existing elements. This is mainly used when calling createGrid again to resize the grid.
+    while(grid.firstChild){
+        grid.removeChild(grid.lastChild);
+    }
+
+    // Use a for loop of the grid's size to create the rows and column elements, using DOM to add in the proper classes.
     for(i = 0; i < gridSize; i++){
         const row = document.createElement('div');
         row.classList.add('row');
@@ -65,6 +70,16 @@ function reset() {
 Set the gridSize var to the inputted number, executing the reset() function to reset the grid.
 */
 function resize(){
+    let message = prompt("Enter a number between 1 and 100 for the grid:");
 
+    if(message <= 100 && message >= 1){
+        reset();
+
+        gridSize = message;
+
+        createGrid();
+    }else{
+        alert("Error! A incorrect input was entered.");
+    }
 }
 
